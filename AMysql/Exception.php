99cdb6f251c45769a535a5d26299e013389e6a26
Szerémi Attila <attila.szeremi@stylersonline.com>
2012-06-28 10:57:44 +0200

    version 0.9.1.1

--- AMysql/Exception.php
+++ AMysql/Exception.php
@@ -2,7 +2,7 @@
 /**
  * Mysql kivétel osztály
  * @author Szerémi Attila 
- * @version 0.9.1
+ * @version 0.9.1.1
  **/ 
 class AMysql_Exception extends RuntimeException {
 
--- AMysql/Expr.php
+++ AMysql/Expr.php
@@ -8,7 +8,7 @@
  *
  * @author Szerémi Attila
  * @created 2011.06.10. 13:26:56  
- * @version 0.9.1
+ * @version 0.9.1.1
  **/ 
 class AMysql_Expr {
 
