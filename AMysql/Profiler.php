62964b16bcc3f238877b8afe2a3013af265f77bf
Attila Szeremi <attila.szeremi@netefficiency.co.uk>
2013-12-13 16:20:43 +0000

    AMysql new shipping with a Profiler template and $profiler->getAsHtml()

--- AMysql/Profiler.php
+++ AMysql/Profiler.php
@@ -18,7 +18,7 @@
  * @author      Szerémi Attila
  * @license     MIT License; http://www.opensource.org/licenses/mit-license.php
  */
-class AMysql_Profiler extends ArrayObject
+class AMysql_Profiler implements ArrayAccess
 {
     protected $amysql;
 
@@ -32,11 +32,50 @@ class AMysql_Profiler extends ArrayObject
         $this->amysql->profileQueries = !!$enabled;
     }
 
-    public function toArray()
+    /**
+     * Gets the profile data as an HTML table with by a template shipped with
+     * this library.
+     * Can also be called with the help of ArrayAccess via $this['asHtml']
+     * 
+     * @access public
+     * @return string
+     */
+    public function getAsHtml()
+    {
+        if (!$encoding) {
+            $encoding = $this->defaultEncoding;
+        }
+        $tplBaseDir = dirname(__FILE__) . '/../tpl';
+        $filename = "$tplBaseDir/profileTemplate.php";
+        $profiler = $this;
+        ob_start();
+        include $filename;
+        $html = ob_get_clean();
+        return $html;
+    }
+
+    /**
+     * Gets the profile data as an array.
+     * Can also be called with the help of ArrayAccess via $this['asArray']
+     * 
+     * @access public
+     * @return array
+     */
+    public function getAsArray()
     {
         return array(
             'totalTime' => $this['totalTime'],
             'queriesData' => $this['queriesData']
+        );
+    }
+
+    public function offsetExists($key)
+    {
+        return in_array(
+            $key,
+            array(
+                'asHtml', 'totalTime', 'queriesData'
+            )
         );
     }
 
@@ -49,10 +88,30 @@ class AMysql_Profiler extends ArrayObject
             case 'queriesData':
                 return $this->amysql->getQueriesData();
                 break;
+            case 'asHtml':
+                return $this->getAsHtml();
+                break;
+            case 'asArray':
+                return $this->getAsArray();
+                break;
             default:
                 trigger_error("Invalid key: `$key`.");
                 break;
         }
+    }
+
+    public function offsetSet($key, $value)
+    {
+        trigger_error("Access denied. Cannot set key `$key`.", E_USER_WARNING);
+    }
+
+    public function offsetUnset($key)
+    {
+        trigger_error(
+            "Access denied. Cannot unset key `$key`.",
+            E_USER_WARNING
+        );
+        return false;
     }
 
     public function __get($key)
